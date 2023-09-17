1b13b86b918b84a708bfa5040a44caf6b766464c
Hercules Chrysanthos <h.chrysanthos>
2023-09-17 21:03:44 +0300

    Add initial MAKES and FUEL_TYPES

--- api/constants.js
+++ api/constants.js
@@ -3,6 +3,19 @@ const ROLES = Object.freeze({
   RENTER: 'renter',
 });
 
+// TODO add more makes
+const MAKES = Object.freeze({
+  AUDI: 'audi',
+});
+
+const FUEL_TYPES = Object.freeze({
+  PETROL: 'Βενζίνη',
+  DIESEL: 'Πετρέλαιο',
+  GAS: 'Αέριο',
+});
+
 module.exports = {
   ROLES,
+  MAKES,
+  FUEL_TYPES,
 };
