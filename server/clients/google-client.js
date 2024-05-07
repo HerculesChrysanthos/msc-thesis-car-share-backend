fde85a5315e3ad7dacbed83b2a9848f3795e9d59
HerculesChrysanthos <hercsma@hotmail.com>
2024-05-07 20:20:27 +0300

    Add google-client

---
+++ server/clients/google-client.js
@@ -0,0 +1,45 @@
+const passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+const userRepository = require('../../api/user/user.repository');
+
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:8080/api/users/google/callback',
+      passReqToCallback: true,
+    },
+    async function (req, accessToken, refreshToken, profile, cb) {
+      console.log('Role parameter:', req.query.state);
+      //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //     return cb(err, user);
+      //   });
+
+      const googleUser = {
+        name: profile._json.given_name,
+        surname: profile._json.family_name,
+        email: profile._json.email,
+        googleId: profile._json.sub,
+        profilePictureUrl: profile._json.picture,
+        role: req.query.state,
+      };
+
+      const dbUser = await userRepository.createOrUpdateUserByGoogleId(
+        googleUser
+      );
+
+      req.dbUser = dbUser;
+
+      return cb(null, profile);
+    }
+  )
+);
