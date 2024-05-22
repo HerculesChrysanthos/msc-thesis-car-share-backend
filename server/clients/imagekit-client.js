1b4ca9074e34bc614974c9a1c3c2429bdbb2a547
HerculesChrysanthos <hercsma@hotmail.com>
2024-05-22 22:48:43 +0300

    Add image kit client

---
+++ server/clients/imagekit-client.js
@@ -0,0 +1,36 @@
+const ImageKit = require('imagekit');
+require('dotenv').config();
+
+async function uploadImage(buffer, name) {
+  try {
+    const imagekit = new ImageKit({
+      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: 'https://ik.imagekit.io/cookhub/',
+    });
+
+    return new Promise((resolve, reject) => {
+      imagekit.upload(
+        {
+          file: buffer,
+          fileName: name,
+          useUniqueFileName: false,
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        }
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+module.exports = {
+  uploadImage,
+};
