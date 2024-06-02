4b7ecce7d2ebf4e3f82542da0082e28c7603cd78
HerculesChrysanthos <hercsma@hotmail.com>
2024-06-02 20:21:34 +0300

    Add hasCarAccess middleware

--- server/middleware/check-auth.js
+++ server/middleware/check-auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const userRepository = require('../api/user/user.repository');
+const utils = require('../utils');
+const carRepository = require('../api/car/car.repository');
 
 const auth = () => {
   return async (req, res, next) => {
@@ -60,7 +62,38 @@ function authorization() {
   };
 }
 
+async function hasCarAccess(req, res, next) {
+  try {
+    const carId = req.params.carId;
+
+    if (!utils.isValidObjectId(carId)) {
+      const error = new Error('Το αυτοκίνητο δε βρέθηκε');
+      error.status = 404;
+      throw error;
+    }
+
+    const car = await carRepository.findCarById(carId);
+
+    if (!car) {
+      const error = new Error('Το αυτοκίνητο δε βρέθηκε');
+      error.status = 404;
+      throw error;
+    }
+
+    if (car.owner.toString() !== req.user._id.toString()) {
+      const error = new Error('Δεν έχεις πρόσβαση');
+      error.status = 403;
+      throw error;
+    }
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+}
+
 module.exports = {
   auth,
   authorization,
+  hasCarAccess,
 };
