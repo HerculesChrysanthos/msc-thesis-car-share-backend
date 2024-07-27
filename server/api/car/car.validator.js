4ac5ece3170be67d680202790505e54c77e1b2e7
HerculesChrysanthos <hercsma@hotmail.com>
2024-07-27 17:58:28 +0300

    Change type on address postalcode

--- server/api/car/car.validator.js
+++ server/api/car/car.validator.js
@@ -151,7 +151,7 @@ const updateCarSpecificFieldsSchema = Joi.object({
       city: Joi.string().required(),
       street: Joi.string().required(),
       number: Joi.number().required(),
-      postalCode: Joi.number().required(),
+      postalCode: Joi.string().required(),
       lat: Joi.number().min(-90).max(90).required(),
       long: Joi.number().min(-180).max(180).required(),
     }),
