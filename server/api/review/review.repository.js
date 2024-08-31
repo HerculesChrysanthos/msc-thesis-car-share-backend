33355eb1aa9fc048bf66fd47e7e7e76e3384215e
HerculesChrysanthos <hercsma@hotmail.com>
2024-08-31 19:20:10 +0300

    Add arrays about as owner and as renter reviews on get user reviews

--- server/api/review/review.repository.js
+++ server/api/review/review.repository.js
@@ -13,9 +13,123 @@ async function getUserReviews(userId, skip, limit) {
       },
     },
     {
+      $lookup: {
+        from: 'users',
+        localField: 'reviewer',
+        foreignField: '_id',
+        as: 'reviewer',
+      },
+    },
+    {
+      $unwind: '$reviewer',
+    },
+    {
+      $lookup: {
+        from: 'bookings',
+        localField: 'booking',
+        foreignField: '_id',
+        as: 'booking',
+      },
+    },
+    {
+      $unwind: '$booking',
+    },
+    {
       $facet: {
-        totalCount: [{ $count: 'count' }],
-        paginatedResults: [{ $skip: skip }, { $limit: limit }],
+        reviewsAsRenter: [
+          {
+            $match: {
+              $expr: {
+                $eq: [{ $toString: '$booking.renter' }, userId],
+              },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalCount: { $sum: 1 },
+              docs: { $push: '$$ROOT' },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              totalCount: 1,
+              paginatedResults: {
+                $slice: [
+                  {
+                    $map: {
+                      input: '$docs',
+                      as: 'doc',
+                      in: {
+                        _id: '$$doc._id',
+                        reviewer: {
+                          _id: '$$doc.reviewer._id',
+                          name: '$$doc.reviewer.name',
+                          surname: '$$doc.reviewer.surname',
+                          email: '$$doc.reviewer.email',
+                        },
+                        reviewedUser: '$$doc.reviewedUser',
+                        booking: '$$doc.booking',
+                        rating: '$$doc.rating',
+                        comment: '$$doc.comment',
+                      },
+                    },
+                  },
+                  skip,
+                  limit,
+                ],
+              },
+            },
+          },
+        ],
+        reviewsAsOwner: [
+          {
+            $match: {
+              $expr: {
+                $ne: [{ $toString: '$booking.renter' }, userId],
+              },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalCount: { $sum: 1 },
+              docs: { $push: '$$ROOT' },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              totalCount: 1,
+              paginatedResults: {
+                $slice: [
+                  {
+                    $map: {
+                      input: '$docs',
+                      as: 'doc',
+                      in: {
+                        _id: '$$doc._id',
+                        reviewer: {
+                          _id: '$$doc.reviewer._id',
+                          name: '$$doc.reviewer.name',
+                          surname: '$$doc.reviewer.surname',
+                          email: '$$doc.reviewer.email',
+                        },
+                        reviewedUser: '$$doc.reviewedUser',
+                        booking: '$$doc.booking',
+                        rating: '$$doc.rating',
+                        comment: '$$doc.comment',
+                      },
+                    },
+                  },
+                  skip,
+                  limit,
+                ],
+              },
+            },
+          },
+        ],
       },
     },
   ]);
