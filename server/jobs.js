4c569804e3d3126e4fa90b4e4cba089c7688d0d7
HerculesChrysanthos <hercsma@hotmail.com>
2024-08-31 13:31:36 +0300

    Add log and comment

--- server/jobs.js
+++ server/jobs.js
@@ -6,10 +6,16 @@ async function markBookingAsDone() {
   const results =
     await bookingService.setAsDoneAcceptedBookingsThatEndDatePassed();
   console.log('markBookingAsDone finished ', new Date());
-  console.log(results);
+  console.log('marked as done ', results?.matchedCount);
 }
 
+// na koitazei bookings pou ksekinoun se 2 hmeres kai na kanei auto reject
+
+// elegxo an uparxei kapoia se mia wra mprosta
+
 function scheduleJobs() {
+  // '* * * * *' run every minute
+  // '1 * * * *' Schedule the job to run at the first minute of every hour
   cron.schedule('* * * * *', markBookingAsDone);
 }
 
